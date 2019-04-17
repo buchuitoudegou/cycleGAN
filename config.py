bc196fe48e43ca7d4655e4d356319197b9871485
unknown <756541536@qq.com>
2019-04-17 13:03:09 +0800

    update config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 data_root = './vangogh2photo/'
 ###############################
-batch_size = 3
+batch_size = 4
 lr = 0.0002
 momentum1 = 0.5
 momentum2 = 0.999
-epoches = 200
+epoches = 10
 cyc = 10.0
 lid = 5.0
