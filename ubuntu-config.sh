f7a8e1b579d289000a3ef94cd94559a16d016991
unknown <756541536@qq.com>
2019-04-22 22:38:13 +0800

    update config

--- ubuntu-config.sh
+++ ubuntu-config.sh
@@ -1,6 +1,10 @@
+echo '> update apt-get'
 apt-get update
-apt-get install python3.7
+echo '> install python3.6'
+apt-get install python3.6
+echo '> install dev'
 pip3 install torch torchvision
 pip3 install matplotlib opencv-python
+echo '> fix cv2'
 apt-get update
 apt-get install libgtk2.0-dev
